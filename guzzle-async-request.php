a5c24d0887a51bc83de5c902c0df31811b047b8f
Tuan Canh <54563697+tuancanhit@users.noreply.github.com>
2023-10-30 15:19:52 +0700

    Update guzzle-async-request.php

--- guzzle-async-request.php
+++ guzzle-async-request.php
@@ -1,5 +1,6 @@
 <?php
 
+// CLIENT
 use GuzzleHttp\Client;
 use GuzzleHttp\Promise;
 
@@ -7,10 +8,10 @@ $client = new Client(['base_uri' => 'http://127.0.0.1']);
 
 // Initiate each request but do not block
 $promises = [
-    'image' => $client->getAsync('/'),
-    'png'   => $client->getAsync('/'),
-    'jpeg'  => $client->getAsync('/'),
-    'webp'  => $client->getAsync('/')
+    'image' => $client->getAsync('/?type=image'),
+    'png'   => $client->getAsync('/?type=png'),
+    'jpeg'  => $client->getAsync('/?type=jpeg'),
+    'webp'  => $client->getAsync('/?type=webp')
 ];
 
 $responses = Promise\Utils::unwrap($promises);
@@ -19,3 +20,23 @@ foreach ($responses as $key => $response) {
     echo $key .': '.$response->getBody()->getContents();
     echo "\n";
 }
+
+?>
+
+<?php
+
+// SERVER
+sleep(1);
+$now = DateTime::createFromFormat('U.u', microtime(true));
+$now = $now->format("Y-m-d H:i:s.u");
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode(['execute_time' => $now]);
+
+?>
+
+Result:
+
+image: {"execute_time":"2023-10-30 08:16:55.102500"}
+png: {"execute_time":"2023-10-30 08:16:55.105900"}
+jpeg: {"execute_time":"2023-10-30 08:16:55.105800"}
+webp: {"execute_time":"2023-10-30 08:16:55.312200"}
